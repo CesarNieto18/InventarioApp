56c8be4071b97c46064b0ebdcf37a2ca7632362b
CESAR AUGUSTO NIETO RUSSI <cesar.nieto@correo.policia.gov.co>
2026-04-06 15:06:44 -0500

    Primer push con estructura base de proyecto y configuracion de gitignore

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// =====================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+//======================================
+
+
+using System.Threading.Channels;
+
+Console.WriteLine("==========================================");
+Console.WriteLine("-----SISTEMA DE GESTIÓN DE INVENTARIO-----");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Versión: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estado: Proyecto inicilalizado");
+
