1d387753c96727c9673d970433b962a60893517e
CESAR AUGUSTO NIETO RUSSI <cesar.nieto@correo.policia.gov.co>
2026-04-06 15:54:25 -0500

    Nueva carpeta models

--- Program.cs
+++ Program.cs
@@ -4,15 +4,29 @@
 //======================================
 
 
-using System.Threading.Channels;
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
 
 Console.WriteLine("==========================================");
 Console.WriteLine("-----SISTEMA DE GESTIÓN DE INVENTARIO-----");
 Console.WriteLine("==========================================");
 Console.WriteLine();
-Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Versión: {version}");
 Console.WriteLine($"Plataforma: {Environment.OSVersion}");
 Console.WriteLine($".NET Versión: {Environment.Version}");
 Console.WriteLine();
-Console.WriteLine("Estado: Proyecto inicilalizado");
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine(" InventarioApp/");
+Console.WriteLine(" |-- Program.cs");
+Console.WriteLine(" |-- Inventario.csproj");
+Console.WriteLine(" |-- README");
+Console.WriteLine(" |-- src/");
+Console.WriteLine("     |--Models");
+Console.WriteLine("Configuración.csproj");
+Console.WriteLine("Carpeta src/creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine("");
+Console.WriteLine("Proximo paso: Checkpoint");
 
